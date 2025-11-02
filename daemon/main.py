649b9395dc4caa201cfdc01a7717ceccbe669196
manuel deneu <manuel.deneu@gmail.com>
2025-11-02 15:57:34 +0100

    daemon: more

--- daemon/main.py
+++ daemon/main.py
@@ -7,21 +7,56 @@ import time
 client = udp_client.SimpleUDPClient("127.0.0.1", 4000)
 
 
-def onVoltageRatioChange(self, voltageRatio):
-    print("VoltageRatio: " + str(voltageRatio))
+def onVoltageRatioChange0(self, voltageRatio):
+    client.send_message("/voltage", 0, voltageRatio)
+
+
+def onVoltageRatioChange1(self, voltageRatio):
+    client.send_message("/voltage", 1, voltageRatio)
+
+
+def onVoltageRatioChange2(self, voltageRatio):
+    client.send_message("/voltage", 2, voltageRatio)
+
+
+def onVoltageRatioChange3(self, voltageRatio):
+    client.send_message("/voltage", 3, voltageRatio)
 
 
 def main():
-    ch = VoltageRatioInput()
-    ch.setChannel(0)
-    ch.setOnVoltageRatioChangeHandler(onVoltageRatioChange)
-    ch.openWaitForAttachment(1000)
+    ch0 = VoltageRatioInput()
+    ch0.setChannel(0)
+    ch0.setOnVoltageRatioChangeHandler(onVoltageRatioChange0)
+    ch0.openWaitForAttachment(1000)
+    ch0.setDataInterval(1000)
+
+    ch1 = VoltageRatioInput()
+    ch1.setChannel(1)
+    ch1.setOnVoltageRatioChangeHandler(onVoltageRatioChange1)
+    ch1.openWaitForAttachment(1000)
+    ch1.setDataInterval(1000)
+
+    ch2 = VoltageRatioInput()
+    ch2.setChannel(2)
+    ch2.setOnVoltageRatioChangeHandler(onVoltageRatioChange2)
+    ch2.openWaitForAttachment(1000)
+    ch2.setDataInterval(1000)
+
+    ch3 = VoltageRatioInput()
+    ch3.setChannel(3)
+    ch3.setOnVoltageRatioChangeHandler(onVoltageRatioChange3)
+    ch3.openWaitForAttachment(1000)
+    ch3.setDataInterval(1000)
+
     while True:
         print("send")
         client.send_message("/filter", 10)
         time.sleep(1)
 
-    ch.close()
+    ch0.close()
+    ch1.close()
+    ch2.close()
+    ch3.close()
 
 
 if __name__ == "__main__":
